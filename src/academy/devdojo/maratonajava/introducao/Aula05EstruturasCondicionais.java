dc41be58a48d85fd97607ad5dec5ae169d407528
kevin <kevinsipioni13@gmail.com>
2024-12-04 10:48:53 -0300

    commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        // !
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Maior de idade");
+        }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Menor de idade");
+        }
+
+        System.out.println("fora do if");
+
+        boolean c = true;
+        if (c == false) {
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+    }
+}
