28e528f78d38cb42b3608b36b9207577ccad0570
kevin <kevinsipioni13@gmail.com>
2024-12-05 01:52:08 -0300

    commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionaisDESAFIO.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionaisDESAFIO {
+    public static void main(String[] args) {
+        double salarioAnual = 70000;
+        double imposto1 = 9.70 / 100;
+        double imposto2 = 37.35 / 100;
+        double imposto3 = 49.50 / 100;
+        double valorImposto;
+
+        if (salarioAnual <= 34712) {
+            valorImposto = salarioAnual *= imposto1;
+        } else if (salarioAnual >= 34712 && salarioAnual <= 68507) {
+            valorImposto = salarioAnual *= imposto2;
+        } else {
+            valorImposto = salarioAnual *= imposto3;
+        }
+        System.out.println(valorImposto);
+    }
+
+
+}
