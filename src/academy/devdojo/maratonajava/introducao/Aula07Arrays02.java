bc8e45c796274a53d62837d4100234cf5a698665
kevin <kevinsipioni13@gmail.com>
2024-12-10 01:05:00 -0300

    commit

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -7,9 +7,13 @@ public class Aula07Arrays02 {
         // boolean false
         // String null
 
-        int[] nomes = new int[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String[] nomes = new String[3];
+        nomes[0] = "Maria";
+        nomes[1] = "João";
+        nomes[2] = "Pedro";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
